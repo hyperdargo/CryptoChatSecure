9f7907a6751978864c4a5bee78dc7b0743bc43d0
DargoTamber <102357016+hyperdargo@users.noreply.github.com>
2025-06-23 16:06:43 +0545

    Update app.py

--- app.py
+++ app.py
@@ -449,4 +449,4 @@ def download_file(file_id):
         return redirect(url_for('chat'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
